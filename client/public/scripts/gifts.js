478fb0b80076356a9b6f61d0b8ca818fa8c44378
Vn0m <juanpapolo123@gmail.com>
2025-09-23 22:13:04 -0400

    add details for specific products

--- client/public/scripts/gifts.js
+++ client/public/scripts/gifts.js
@@ -1,7 +1,7 @@
 const renderGifts = async () => {
     const response = await fetch('/gifts')
     const data = await response.json()
-    
+
     const mainContent = document.getElementById('main-content')
     if (data) {
         data.map(gift => {
@@ -39,3 +39,29 @@ const renderGifts = async () => {
 }
 
 renderGifts()
+
+const renderGift = async () => {
+    const requestedID = parseInt(window.location.href.split('/').pop())
+    const response = await fetch('/gifts')
+    const data = await response.json()
+    const giftContent = document.getElementById('gift-content')
+    let gift
+    if(data){
+        gift = data.find(gift => gift.id === requestedID)
+        if(gift){
+            document.getElementById('image').src = gift.image
+            document.getElementById('name').textContent = gift.name
+            document.getElementById('submittedBy').textContent = 'Submitted by: ' + gift.submittedBy
+            document.getElementById('pricePoint').textContent = 'Price: ' + gift.pricePoint
+            document.getElementById('audience').textContent = 'Great For: ' + gift.audience
+            document.getElementById('description').textContent = gift.description
+            document.title = `UnEarthed - ${gift.name}`
+        }else{
+            const message = document.createElement('h2')
+            message.textContent = 'No Gifts Available '
+            giftContent.appendChild(message)
+        }
+    }
+}
+
+renderGift()
